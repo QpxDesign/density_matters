12a96832458dd009798681d0408beda532cf6c7b
Quinn555 <71609644+Quinn555@users.noreply.github.com>
2023-05-17 13:05:36 -0400

    added reverse feature to density calculator and changed development map icon

--- src/calculators/PopulationDensity.tsx
+++ src/calculators/PopulationDensity.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import {BsHouseFill,BsFillBuildingFill,BsBuildingsFill} from "react-icons/bs"
-import {GiModernCity} from "react-icons/gi"
+import { BsHouseFill, BsFillBuildingFill, BsBuildingsFill } from "react-icons/bs"
+import { GiModernCity } from "react-icons/gi"
 
 export default function PopulationDensity() {
 
@@ -10,30 +10,53 @@ export default function PopulationDensity() {
     const MIDTOWN_MANHATTAN_POP_DENSITY = 46_000 // PEOPLE PER MILE (https://www.nyc.gov/site/planning/planning-level/nyc-population/current-future-populations.page)
 
     const ACRES_TO_SQUARE_MILES = 640;
+    const DC_SQUARE_MILES = 68.35;
 
     const [yardSize, setYardSize]: any = useState(0)
+    const [reversed, setReversed]: any = useState(false)
     return (
         <div style={{ width: "min(30em,90%)", background: "#a5b4fc", margin: "1em auto", }}>
             <div className="h-stack-around">
-                <label style={{ padding: '.65em' }}><h3>Enter the size of your yard (in acres):</h3></label>
-                <input style={{ width: "30%", border: "none", fontSize: "1.5em", padding: ".25em .5em", fontFamily: "pt-mono", marginRight: ".5em" }} type="number" onChange={(e) => setYardSize(e.target.value)} />
+                <label style={{ padding: '.65em' }}><h3>{reversed ? "Enter a Population (in hundreds of thousands)" : "Enter the size of your yard (in acres):"}</h3></label>
+                <input style={{ width: "25%", border: "none", fontSize: "1.5em", padding: ".25em .5em", fontFamily: "pt-mono", marginRight: ".5em" }} type="number" onChange={(e) => setYardSize(e.target.value)} />
+                <button style={{ fontSize: "1em", margin: 0, marginRight: ".25em" }} onClick={() => {
+                    if (reversed) { setReversed(false) } else {
+                        setReversed(true)
+                    }
+                }}>REVERSE</button>
             </div>
-            <div className="h-stack-around" style={{ background: "#818cf8", width: "100%"}}><h3 style={{ padding: '.65em',gap:'.5em',alignItems:"center"}} className='h-stack'>
-                <BsHouseFill className='icon'/>Density of Houston⁶</h3>
-                <h3 style={{marginRight:".75em",fontWeight:100}}>{Math.floor(yardSize/ACRES_TO_SQUARE_MILES * HOUSTON_POP_DENSITY)} people</h3>
+            {reversed ? <><div className="h-stack-around" style={{ background: "#818cf8", width: "100%" }}><h3 style={{ padding: '.65em', gap: '.5em', alignItems: "center" }} className='h-stack'>
+                <BsHouseFill className='icon' />Density of Houston⁶</h3>
+                <h3 style={{ marginRight: ".75em", fontWeight: 100 }}>{Math.round(((yardSize * 100_000) / HOUSTON_POP_DENSITY / DC_SQUARE_MILES) * 100)/100} DCs</h3>
             </div>
-            <div className="h-stack-around" style={{ background: "#818cf8", width: "100%"}}><h3 style={{ padding: '.65em',gap:'.5em',alignItems:"center"}} className='h-stack'>
-                <BsFillBuildingFill className='icon'/>Density of Georgetown⁷</h3>
-                <h3 style={{marginRight:".75em",fontWeight:100}}>{Math.floor(yardSize/ACRES_TO_SQUARE_MILES * GEORGETOWN_POP_DENSITY)} people</h3>
-            </div>
-            <div className="h-stack-around" style={{ background: "#818cf8", width: "100%"}}><h3 style={{ padding: '.65em',gap:'.5em',alignItems:"center"}} className='h-stack'>
-                <BsBuildingsFill className='icon'/>Density of San Francisco⁶</h3>
-                <h3 style={{marginRight:".75em",fontWeight:100}}>{Math.floor(yardSize/ACRES_TO_SQUARE_MILES * SAN_FRANCISCO_POP_DENSITY)} people</h3>
-            </div>
-            <div className="h-stack-around" style={{ background: "#818cf8", width: "100%"}}><h3 style={{ padding: '.65em',gap:'.5em',alignItems:"center"}} className='h-stack'>
-                <GiModernCity className='icon'/>Density of Manhattan⁶</h3>
-                <h3 style={{marginRight:".75em",fontWeight:100}}>{Math.floor(yardSize/ACRES_TO_SQUARE_MILES * MIDTOWN_MANHATTAN_POP_DENSITY)} people</h3>
-            </div>
+                <div className="h-stack-around" style={{ background: "#818cf8", width: "100%" }}><h3 style={{ padding: '.65em', gap: '.5em', alignItems: "center" }} className='h-stack'>
+                    <BsFillBuildingFill className='icon' />Density of Georgetown⁷</h3>
+                    <h3 style={{ marginRight: ".75em", fontWeight: 100 }}>{Math.round(((yardSize * 100_000) / GEORGETOWN_POP_DENSITY / DC_SQUARE_MILES) * 100)/100} DCs</h3>
+                </div>
+                <div className="h-stack-around" style={{ background: "#818cf8", width: "100%" }}><h3 style={{ padding: '.65em', gap: '.5em', alignItems: "center" }} className='h-stack'>
+                    <BsBuildingsFill className='icon' />Density of San Francisco⁶</h3>
+                    <h3 style={{ marginRight: ".75em", fontWeight: 100 }}>{Math.round(((yardSize * 100_000) / SAN_FRANCISCO_POP_DENSITY / DC_SQUARE_MILES) * 100)/100} DCs</h3>
+                </div>
+                <div className="h-stack-around" style={{ background: "#818cf8", width: "100%" }}><h3 style={{ padding: '.65em', gap: '.5em', alignItems: "center" }} className='h-stack'>
+                    <GiModernCity className='icon' />Density of Manhattan⁶</h3>
+                    <h3 style={{ marginRight: ".75em", fontWeight: 100 }}>{Math.round(((yardSize * 100_000) / MIDTOWN_MANHATTAN_POP_DENSITY / DC_SQUARE_MILES)*100)/100} DCs</h3>
+                </div> </> : <> <div className="h-stack-around" style={{ background: "#818cf8", width: "100%" }}><h3 style={{ padding: '.65em', gap: '.5em', alignItems: "center" }} className='h-stack'>
+                    <BsHouseFill className='icon' />Density of Houston⁶</h3>
+                    <h3 style={{ marginRight: ".75em", fontWeight: 100 }}>{Math.floor(yardSize / ACRES_TO_SQUARE_MILES * HOUSTON_POP_DENSITY)} people</h3>
+                </div>
+                <div className="h-stack-around" style={{ background: "#818cf8", width: "100%" }}><h3 style={{ padding: '.65em', gap: '.5em', alignItems: "center" }} className='h-stack'>
+                    <BsFillBuildingFill className='icon' />Density of Georgetown⁷</h3>
+                    <h3 style={{ marginRight: ".75em", fontWeight: 100 }}>{Math.floor(yardSize / ACRES_TO_SQUARE_MILES * GEORGETOWN_POP_DENSITY)} people</h3>
+                </div>
+                <div className="h-stack-around" style={{ background: "#818cf8", width: "100%" }}><h3 style={{ padding: '.65em', gap: '.5em', alignItems: "center" }} className='h-stack'>
+                    <BsBuildingsFill className='icon' />Density of San Francisco⁶</h3>
+                    <h3 style={{ marginRight: ".75em", fontWeight: 100 }}>{Math.floor(yardSize / ACRES_TO_SQUARE_MILES * SAN_FRANCISCO_POP_DENSITY)} people</h3>
+                </div>
+                <div className="h-stack-around" style={{ background: "#818cf8", width: "100%" }}><h3 style={{ padding: '.65em', gap: '.5em', alignItems: "center" }} className='h-stack'>
+                    <GiModernCity className='icon' />Density of Manhattan⁶</h3>
+                    <h3 style={{ marginRight: ".75em", fontWeight: 100 }}>{Math.floor(yardSize / ACRES_TO_SQUARE_MILES * MIDTOWN_MANHATTAN_POP_DENSITY)} people</h3>
+                </div></>}
+
         </div>
     )
 }
